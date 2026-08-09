354dfd7334adfa035562c1e16a1f2e1e283a5cf0
jesswinanto1 <jesswinanto1@gmail.com>
2026-08-09 14:21:29 +0530

    Add files via upload

---
+++ 10_data_cleanup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+directory="/path/to/cleanup"
+
+find "$directory" -type f -mtime +7 -exec rm {} \;
+echo "Old files removed."
---
+++ 29_network_interface_information.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+network_interface="eth0"
+
+ifconfig "$network_interface"
+echo "Network interface information displayed."
